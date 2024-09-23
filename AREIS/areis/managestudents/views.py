08c3fb0886f71e97b1df3672c5a2b0ac48877b29
Mark <164849573+MarkyPot@users.noreply.github.com>
2024-09-23 18:41:58 +0800

    manage student now displays filtered student list

--- AREIS/areis/managestudents/views.py
+++ AREIS/areis/managestudents/views.py
@@ -13,7 +13,12 @@ def course_list(request):
     return render(request, 'managestudents/trigger_course_list.html', { 'courses' : courses }) #'courses' : courses this is a dictionary datatype
 
 
-def trigger_students_list(request):
-    students = Students.objects.all()
+def trigger_students_list(request, CatalogueNo):
+    #students = Students.objects.all()
+    studentsfilter = Studentgrades.objects.filter(catalogueno = CatalogueNo)
+    # Extract student IDs from the studentsfilter queryset
+    student_ids = studentsfilter.values_list('studentid', flat=True)
+    # Filter Students based on the extracted student IDs
+    students = Students.objects.filter(studentid__in=student_ids)
     #first - request (the link I think ie about, home, posts), second - html file location, third - setting the value of 'courses' as the courses object this will be used in the html file
     return render(request, 'managestudents/trigger_students_list.html', { 'students' : students })
