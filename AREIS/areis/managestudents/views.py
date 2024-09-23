ea2d73edd70d91adf086bc3a34b13e96b7760038
Mark <164849573+MarkyPot@users.noreply.github.com>
2024-09-23 19:35:13 +0800

    managestudent updated

--- AREIS/areis/managestudents/views.py
+++ AREIS/areis/managestudents/views.py
@@ -15,10 +15,19 @@ def course_list(request):
 
 def trigger_students_list(request, CatalogueNo):
     #students = Students.objects.all()
-    studentsfilter = Studentgrades.objects.filter(catalogueno = CatalogueNo)
+    studentsgrades = Studentgrades.objects.filter(catalogueno = CatalogueNo)
     # Extract student IDs from the studentsfilter queryset
-    student_ids = studentsfilter.values_list('studentid', flat=True)
+    student_ids = studentsgrades.values_list('studentid', flat = True)
     # Filter Students based on the extracted student IDs
-    students = Students.objects.filter(studentid__in=student_ids)
+    students = Students.objects.filter(studentid__in = student_ids)
     #first - request (the link I think ie about, home, posts), second - html file location, third - setting the value of 'courses' as the courses object this will be used in the html file
-    return render(request, 'managestudents/trigger_students_list.html', { 'students' : students })
+    course = Courses.objects.get(catalogueno = CatalogueNo)
+    
+    #print(studentsgrades)
+
+    context = {
+        'students': students,
+        'studentsgrades': studentsgrades,
+        'course': course
+    }
+    return render(request, 'managestudents/trigger_students_list.html', context)
